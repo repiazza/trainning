2f8c243b22bcfd256a024fa75717075ae0fb0fc7
Renato <repiazza@gmail.com>
2020-04-04 16:31:34 -0300

    Acao de clique do botao

--- PrimeiroApp/app/src/main/java/br/com/renatofermi/primeiroapp/MainActivity.java
+++ PrimeiroApp/app/src/main/java/br/com/renatofermi/primeiroapp/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.renatofermi.primeiroapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button   btn = findViewById(R.id.button);
+        final TextView txv = findViewById(R.id.textView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ( txv.getText().equals("Hello World!") ){
+                    txv.setText("Ola mundo");
+                }
+                else{
+                    txv.setText("Hello World!");
+                }
+            }
+        });
     }
 }
